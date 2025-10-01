2a756676ead513cb5b63391c9ab968a0226fd85c
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2025-10-01 12:23:46 +0300

    Adjust default values for exaggeration and temperature in synthesis

--- server.py
+++ server.py
@@ -102,8 +102,8 @@ async def synthesize(request: Request):
     language_id = data.get('language_id', 'ar')
     audio_prompt_path = data.get('voice_id')
     cfg_weight = data.get('speed', 1.0) / 2.0  # Speed is inversely related to cfg_weight
-    exaggeration = data.get('exaggeration', 0.6)
-    temperature = data.get('temperature', 0.8)
+    exaggeration = data.get('exaggeration', 0.3)
+    temperature = data.get('temperature', 0.5)
     output_sample_rate = data.get('output_sample_rate', 8000)
     chunk_size = data.get('chunk_size', 50)
 
